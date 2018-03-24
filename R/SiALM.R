fecab1d95bb0bc64c94eb1f3daee1f694877d067
DTichit <damien.tichit@gmail.com>
2018-03-24 21:21:12 +0100

    Lier SiALM avec dplyr et doParallel

---
+++ R/SiALM.R
@@ -0,0 +1,7 @@
+#' SiALM est un package permettant de calculer un best-estimate pour une compagnie d'assurance proposant les prosuits suivants : contrats d'epargne.
+#'
+#' @docType package
+#' @name SiALM
+#' @import utils
+#' @importFrom stats weighted.mean
+NULL
