52753b933a1072ad0890c9c55a953755b393e8bb
DTichit <damien.tichit@gmail.com>
2018-03-27 16:13:36 +0200

    Gestion de la reserve de capitalisation

---
+++ R/ReserveCapi-dotation.R
@@ -0,0 +1,52 @@
+##' Fonction \code{dotation_reserve_capi}.
+##'
+##' Cette fonction permet de doter la Reserve de capitalisation. Sa modelisation reflete sa definition (Article A333.3).
+##'
+##' En cas de plus value (montant > 0) : dotation totale.
+##' En cas de moins value (montant < 0) : reprise dans la reserve de capitalisation, sous reserve qu'il soit encore positif.
+##'
+##' @name dotation_reserve_capi
+##' @docType methods
+##' @param reserve_capi est un objet de type \code{\link{ReserveCapi}}.
+##' @param pmvr est un \code{numeric} representant le montant les plus ou moins value resalisees a doter.
+##' @author Damien Tichit pour Sia Partners
+##' @export
+##' @include ReserveCapi-class.R
+##'
+setGeneric(name = "dotation_reserve_capi", def = function(reserve_capi, pmvr) {standardGeneric("dotation_reserve_capi")})
+setMethod(
+    f = "dotation_reserve_capi",
+    signature = c(reserve_capi = "ReserveCapi", pmvr = "numeric"),
+    definition = function(reserve_capi, pmvr){
+
+
+
+        # Dans le cas de plus value realise : On dote tout
+        if(pmvr >= 0) {
+
+            # Mise a jour de l'attribut
+            reserve_capi@montant <- reserve_capi@montant + pmvr
+
+            # Reste
+            reste_pvm <- 0
+
+        } else {
+
+            # Calcul de la reprise maximale pouvant etre effectue
+            reprise <- min(reserve_capi@montant, abs(pmvr))
+
+            # Mise a jour de l'objet
+            reserve_capi@montant <- reserve_capi@montant - reprise
+
+            # Reste
+            reste_pvm <- abs(pmvr) - reprise
+
+        }
+
+
+
+        # Output
+        return(list(reserve_capi = reserve_capi,
+                    reste_pvm = reste_pvm))
+    }
+)
