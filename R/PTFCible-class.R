6ada8d926996a6022613d62774fba00674844e05
DTichit <damien.tichit@gmail.com>
2018-03-27 21:29:27 +0200

    Suppression des 2 PTF cibles : actions et immo

--- R/PTFCible-class.R
+++ R/PTFCible-class.R
@@ -4,9 +4,7 @@
 ##'
 ##' @name PTFCible
 ##' @docType class
-##' @slot action est un objet de la classe \code{\link{Action}} representant le portfeuille cible action.
 ##' @slot obligation est un objet de la classe \code{\link{Obligation}} representant le portfeuille cible obligation.
-##' @slot immobilier est un objet de la classe \code{\link{Immobilier}} representant le portfeuille cible Immobilier.
 ##' @slot alloc_cible est un objet \code{data.frame} indiquant l'allocation cible pour chaque produit.
 ##' @author Damien Tichit pour Sia Partners
 ##' @keywords classes
@@ -16,9 +14,7 @@
 setClass(
     Class = "PTFCible",
 
-    slots = c(action = "Action",
-              obligation = "Obligation",
-              immobilier = "Immobilier",
+    slots = c(obligation = "Obligation",
               alloc_cible = "data.frame"),
 
     validity = function (object){
@@ -27,9 +23,7 @@ setClass(
         list_err <- NULL
 
         # Tests sur les classes de l'objet
-        if(! validObject(object@action))       list_err <- c(list_err, "[PTFCible] : 'action' n'est pas valide")
         if(! validObject(object@obligation))   list_err <- c(list_err, "[PTFCible] : 'obligation' n'est pas valide")
-        if(! validObject(object@immobilier))   list_err <- c(list_err, "[PTFCible] : 'immobilier' n'est pas valide")
         if(! is.data.frame(object@alloc_cible))   list_err <- c(list_err, "[PTFCible] : 'alloc_cible' n'est pas valide")
 
         # Output
@@ -49,15 +43,11 @@ setMethod(
     f = "initialize",
     signature = "PTFCible",
     definition = function(.Object,
-                          action = "Action",
                           obligation = "Obligation",
-                          immobilier = "Immobilier",
                           alloc_cible = "data.frame"){
 
-        if(! (missing(action) | missing(obligation) | missing(immobilier) | missing(alloc_cible))){
-            .Object@action      <- action
+        if(! ( missing(obligation) | missing(alloc_cible))){
             .Object@obligation  <- obligation
-            .Object@immobilier  <- immobilier
             .Object@alloc_cible <- alloc_cible
 
             # Validation de l'objet
@@ -65,9 +55,7 @@ setMethod(
 
         } else {
             #Traitement du cas vide
-            .Object@action     <- new("Action")
             .Object@obligation  <- new("Obligation")
-            .Object@immobilier     <- new("Immobilier")
             .Object@alloc_cible     <- data.frame()
 
             warnings("[PTFCible] : Attention au moins un des obets est manquant a l'initialisation")
@@ -86,9 +74,7 @@ setMethod(
     signature = "PTFCible",
     definition = function(x, i){
         switch(EXPR = i,
-               "action" = {return(x@action)},
                "obligation" = {return(x@obligation)},
-               "immobilier" = {return(x@immobilier)},
                "alloc_cible" = {return(x@alloc_cible)},
                stop("Cet attribut n'existe pas!")
         )
@@ -104,9 +90,7 @@ setReplaceMethod(
     signature = "PTFCible",
     definition = function(x, i, value){
         switch(EXPR = i,
-               "action" = {x@action <- value},
                "obligation" = {x@obligation <- value},
-               "immobilier" = {x@immobilier <- value},
                "alloc_cible" = {x@alloc_cible <- value},
                stop("Cet attribut n'existe pas!")
         )
