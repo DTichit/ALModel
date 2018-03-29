f875477aebaadea8b3f34bc33e4fc6288c7c0ec0
dtichit <damien.tichit@sia-partners.com>
2018-03-29 18:48:11 +0200

    Ajout d'une "petite" fonction permettant de supprimer les NAs sur plusieurs vecteurs

--- R/fonctions_utiles.R
+++ R/fonctions_utiles.R
@@ -6,3 +6,17 @@ psum <- function(..., na.rm = FALSE) {
     rowSums(do.call(cbind, list(...)), na.rm = na.rm)
 
 }
+
+
+# Fonction qui, prenant 2 vecteurs, supprime les NA
+pna.omit <- function(x, y) {
+
+    # Id etant NA chez y
+    id_na <- which(is.na(y))
+
+    # MaJ de x
+    y[id_na] <- x[id_na]
+
+    # Output
+    return(y)
+}
