304c475ecc55117c91d8489f87748c58793bf440
dtichit <damien.tichit@sia-partners.com>
2018-03-29 18:51:07 +0200

    Rebalancement du PTF obligataire : NON FINI. Reprendre principalement l'achat. La vente est, a priori, OK : a tester

--- R/Obligation-rebalancement.R
+++ R/Obligation-rebalancement.R
@@ -28,20 +28,30 @@ setMethod(
         ptf_cible <- oblig_cible@ptf[order(oblig_cible@ptf[,"id_mp"]), ]
         ptf       <- oblig@ptf[order(oblig@ptf[,"id_mp"]), ]
 
+        # Extraction des donnees du PTF cible
+        names_ptf_cible <- names(ptf_cible)
+        prop_ptf_cible <- .subset2(ptf_cible, which(names_ptf_cible == "prop"))
+        id_mp_cible <- as.character(.subset2(ptf_cible, which(names_ptf_cible == "id_mp")))
+        mat_res_cible <- .subset2(ptf_cible, which(names_ptf_cible == "mat_res"))
+        cle_cible  <- paste(ptf_cible$cible, mat_res_cible, sep = ".")
+
         # Extraction des donnees du PTF
         names_ptf <- names(ptf)
         num_vm_ptf <- which(names_ptf == "valeur_marche")
         vm_ptf <- .subset2(ptf, num_vm_ptf)
-        cible_action <- .subset2(ptf, which(names_ptf == "cible"))
+        id_mp <- as.character(.subset2(ptf, which(names_ptf == "id_mp")))
+        mat_res <- .subset2(ptf, which(names_ptf == "mat_res"))
+        cle  <- paste(ptf$cible, mat_res, sep = ".")
 
-        # Extraction des donnees du PTF cible
-        names_ptf_cible <- names(ptf_cible)
-        vm_ptf_cible <- .subset2(ptf_cible, which(names_ptf_cible == "valeur_marche"))
-        prop_ptf_cible <- .subset2(ptf_cible, which(names_ptf_cible == "prop"))
+        # Presence des ces obligations dans le PTF ?
+        id_pres_ptf <- match(cle_cible, cle)
+        id_pres <- match(cle, cle_cible)
+        id_pres_cib <- id_pres[!is.na(id_pres)]
+
+
 
         # Differentiel
         diff_alloc <- sum(vm_ptf) - alloc_cible
-
 
 
         ## ###########################
@@ -51,9 +61,39 @@ setMethod(
         if(diff_alloc < 0) {
 
             ## ###
-            ## Dans ce cas la, il manque des actions => ACHAT
+            ## Dans ce cas la, manque => ACHAT
             ## ###
 
+            # Presence des cibles dans le PTF
+            if(! all(! is.na(id_pres_ptf))) {
+
+                # Besoin d'ajouter les nouvelles obligs dans le PTF
+                merge <- merge(ptf, ptf_cible, by = c("cible", "mat_res"), all.x = TRUE, all.y = TRUE)
+
+                # Creation du nouveau portefeuille
+                ptf <- data.frame(id_mp = pna.omit(as.character(merge$id_mp.x), as.character(merge$id_mp.y)), mat_res = merge$mat_res, cible = merge$cible,
+                                  valeur_comptable = pna.omit(merge$valeur_comptable, merge$valeur_achat), valeur_marche = pna.omit(merge$valeur_marche, merge$valeur_achat),
+                                  coupon = pna.omit(merge$coupon.x, merge$coupon.y), nominal = pna.omit(merge$nominal.x, merge$nominal.y), prop = merge$prop)
+
+
+            } else {
+
+                # Determination de la quantite a acheter
+                achat <- ptf$prop[id_pres_ptf] * abs(diff_alloc)
+                nb_achat <- achat / ptf_cible$valeur_marche
+
+                # Mise en image de donnees
+                nominal_prev <- ptf$nominal[id_pres_ptf]
+                nominal_achat <- nb_achat * ptf_cible$nominal[id_pres_cib]
+
+                # Mise a jour du PTF
+                ptf$valeur_comptable[id_pres_ptf] <- ptf$valeur_comptable[id_pres_ptf] + achat
+                ptf$valeur_marche[id_pres_ptf] <- ptf$valeur_marche[id_pres_ptf] + achat
+                ptf$coupon[id_pres_ptf] <- (ptf$coupon[id_pres_ptf] * nominal_prev + ptf_cible$coupon[id_pres_cib] * nominal_achat) / (nominal_prev + nominal_achat)
+                ptf$nominal[id_pres_ptf] <- nominal_prev + nominal_achat
+
+
+            }
 
 
             ## ###########################
@@ -63,10 +103,86 @@ setMethod(
         } else {
 
             ## ###
-            ## Dans ce cas la, il manque des actions => VENTE
+            ## Dans ce cas la : trop => VENTE
             ## ###
 
+            # Presence des non-cibles
+            id_ncib <- which(is.na(id_pres))
 
+            if(length(id_ncib) > 0L) {
+
+                # Somme des VM des non-cibles
+                sum_vm_ncib <- sum(ptf$valeur_marche[id_ncib])
+
+                if(sum_vm_ncib > diff_alloc) {
+
+                    # Somme cumulee des VM
+                    cum_sum_vm <- cumsum(ptf$valeur_marche[id_ncib])
+
+                    # ID a supprimer
+                    id_del <- which(cum_sum_vm > diff_alloc)[1L]
+
+                    if((id_del > 1L) & (! is.null(id_del))) {
+
+                        # VM etant supprimees
+                        vm_del <- ptf[1L:(id_del-1L), "valeur_marche"]
+
+                        # Supprimer les lignes du PTF
+                        ptf <- ptf[-(1L:(id_del-1L)),]
+
+                        # Mise a jour du reste a vendre
+                        diff_alloc <- diff_alloc - sum(vm_del)
+
+                    }
+
+                    # Vente d'une partie des VM
+                    ptf[1L, "valeur_marche"] <- ptf[1L, "valeur_marche"] - diff_alloc
+
+                    # Mise a jour du reste a vendre
+                    diff_alloc <- 0
+
+
+                } else {
+
+                    # VM etant supprimees
+                    vm_del <- ptf[id_ncib, "valeur_marche"]
+
+                    # Supprimer les lignes du PTF
+                    ptf <- ptf[-id_ncib,]
+
+                    # Mise a jour du reste a vendre
+                    diff_alloc <- diff_alloc - sum(vm_del)
+
+                }
+
+
+            }
+
+
+            # Vente des non cibles, le cas echeant
+            if(diff_alloc > 0) {
+
+                # Calcul de la vente devant etre effectue
+                vente <- ptf$prop[id_pres_ptf] * abs(diff_alloc)
+                nb_vente <- vente / ptf$valeur_marche[id_pres_ptf]
+
+
+                # Mise en image de donnees
+                nominal_prev <- ptf$nominal[id_pres_ptf]
+                prop_vc <- ptf$valeur_comptable[id_pres_ptf] / ptf$valeur_marche[id_pres_ptf]
+                prop_nom <- ptf$nominal[id_pres_ptf] / ptf$valeur_marche[id_pres_ptf]
+
+                # Mise a jour du PTF
+                ptf$valeur_marche[id_pres_ptf] <- ptf$valeur_marche[id_pres_ptf] - vente
+                ptf$valeur_comptable[id_pres_ptf] <- ptf$valeur_comptable[id_pres_ptf] - vente * prop_vc
+                ptf$nominal[id_pres_ptf] <- ptf$nominal[id_pres_ptf] - vente * prop_vm
+
+
+                # Mise a jour du reste a vendre
+                diff_alloc <- diff_alloc - sum(vente)
+
+
+            }
 
         }
 
