00c34da139d041ddac494f3d1b1ca7a29d260bb6
dtichit <damien.tichit@sia-partners.com>
2018-05-03 12:24:34 +0200

    Modification de la fonction de telle sorte a ce qu'on ait un ratio en sortie

--- R/Passif-calcul_quote_part_fp.R
+++ R/Passif-calcul_quote_part_fp.R
@@ -1,8 +1,8 @@
-##' Fonction \code{resultat_fin_fp}
+##' Fonction \code{calcul_quote_part_fp}
 ##'
-##' Cette fonction permet de determiner le resultat financier des actifs en face les fonds propres.
+##' Cette fonction permet de determiner la proportion que represente les fonds propres sur le passif d'une compagnie d'assurance.
 ##'
-##' @name resultat_fin_fp
+##' @name calcul_quote_part_fp
 ##' @docType methods
 ##' @param passif est un objet de type \code{\link{Passif}}.
 ##' @param result_fin est un \code{numeric} representant le resultat financier total.
@@ -10,15 +10,15 @@
 ##' @export
 ##' @include Passif-class.R
 ##'
-setGeneric(name = "resultat_fin_fp", def = function(passif, result_fin) {standardGeneric("resultat_fin_fp")})
+setGeneric(name = "calcul_quote_part_fp", def = function(passif) {standardGeneric("calcul_quote_part_fp")})
 setMethod(
-    f = "resultat_fin_fp",
-    signature = c(passif = "Passif", result_fin = "numeric"),
-    definition = function(passif, result_fin){
+    f = "calcul_quote_part_fp",
+    signature = c(passif = "Passif"),
+    definition = function(passif){
 
 
         ## ###########################
-        ##      Extraction des PMs
+        ##      Extraction des PT
         ## ###########################
 
         # Determination des PMs totales
@@ -26,6 +26,9 @@ setMethod(
 
         # PM totales
         pm <- sum_list(pm, 1L)
+
+        # PPE totale
+        ppe <- sum(passif@provision@ppe@ppe)
 
 
 
@@ -38,24 +41,14 @@ setMethod(
         fp <- passif@fonds_propres@capital_social + passif@fonds_propres@report_a_nouveau
 
         # Quote-Part capitaux propres
-        qp_cp <- fp / (fp + pm)
-
-
-
-
-        ## ###########################
-        ## Calcul du resulat
-        ## ###########################
-
-        # Resultat financiers des actifs en face des capitaux propres
-        result_fin_fp <- qp_cp * result_fin
+        qp_cp <- fp / (fp + pm + ppe)
 
 
 
 
 
         # Output
-        return(list(result_fin_fp = result_fin_fp))
+        return(qp_cp)
 
 
     }
